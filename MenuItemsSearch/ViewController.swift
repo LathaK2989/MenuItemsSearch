8ac9f116e4c9e43e8d213816b8be66e75697ffec
Srilatha Karancheti <slkarancheti@gmail.com>
2024-09-13 17:59:50 -0400

    Initial Commit

---
+++ MenuItemsSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MenuItemsSearch
+//
+//  Created by Srilatha on 2024-09-13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MenuItemsSearchUITests/MenuItemsSearchUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MenuItemsSearchUITestsLaunchTests.swift
+//  MenuItemsSearchUITests
+//
+//  Created by Srilatha on 2024-09-13.
+//
+
+import XCTest
+
+final class MenuItemsSearchUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
